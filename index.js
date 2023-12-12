c12b7bd30c5de62f158db7657607318b6b0e5c80
Daniel Osaetin <osaetindaniel@gmail.com>
2023-12-12 21:10:52 +0100

    chore: Use const for env variable

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 // This  plugin searches for JIRA tickets (with a predefined prefix)
-// and replaces the prefix with a markdown URL to the Jira ticket
+// and replaces the prefix with a markdown URL to the JIRA ticket
 import { Plugin } from 'release-it'
 
-export default class JIRAReleaseNotes extends Plugin {
+const jiraHostEnvironmentVariable = 'RELEASE_IT_JIRA_NOTES_JIRA_HOST'
+const ticketPrefixesEnvironmentVariable = 'RELEASE_IT_JIRA_NOTES_JIRA_TICKET_PREFIXES'
 
+export default class JIRAReleaseNotes extends Plugin {
 
   static isEnabled(options) {
     let enabled = true
 
-    if (typeof options.jiraHost !== 'string' && !process.env['RELEASE_IT_JIRA_NOTES_JIRA_HOST']) {
+    if (typeof options.jiraHost !== 'string' && !process.env[jiraHostEnvironmentVariable]) {
       console.error(
-        'Plese set the "jiraHost" key or the RELEASE_IT_JIRA_NOTES_JIRA_HOST environment variable'
+        `Plese set the "jiraHost" key or the ${jiraHostEnvironmentVariable} environment variable`
       )
       enabled = false
     }
 
-    if (options.ticketPrefixes?.isArray() === undefined && !process.env['RELEASE_IT_JIRA_NOTES_JIRA_TICKET_PREFIXES']) {
+    if (options.ticketPrefixes?.isArray() === undefined && !process.env[ticketPrefixesEnvironmentVariable]) {
       console.error(
-        `Please set the "ticketPrefixes" key or the RELEASE_IT_JIRA_NOTES_JIRA_TICKET_PREFIXES
+        `Please set the "ticketPrefixes" key or the ${ticketPrefixesEnvironmentVariable}
 environment variable to a comma separated value of strings e.g ABC,DEF`
       )
       enabled = false
@@ -37,27 +39,26 @@ one or more options are missing/misconfigured, Please configure them`
     let jiraHost = options.jiraHost
     let ticketPrefixes = options.ticketPrefixes
 
-    if(jiraHost === undefined) {
-      jiraHost = process.env['RELEASE_IT_JIRA_NOTES_JIRA_HOST']
+    if (jiraHost === undefined) {
+      jiraHost = process.env[jiraHostEnvironmentVariable]
     }
 
     if (ticketPrefixes === undefined) {
-      ticketPrefixes = process.env['RELEASE_IT_JIRA_NOTES_JIRA_TICKET_PREFIXES'].split(',')
+      ticketPrefixes = process.env[ticketPrefixesEnvironmentVariable].split(',')
     }
 
     return Object.assign({}, options[pluginName], {
       jiraHost, ticketPrefixes
     })
-}
+  }
 
   beforeRelease() {
     let changelog = this.config.getContext('changelog')
 
     const { jiraHost, ticketPrefixes } = this.options
-    
+
     for (const prefix of ticketPrefixes) {
       const regex = new RegExp(`(${prefix}-\\w+)`, 'g')
-
       changelog = changelog.replaceAll(regex, `[$1](https://${jiraHost}/browse/$1)`)
     }
 
